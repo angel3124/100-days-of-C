6e188c84c7235c7500cc2577b6e5b06f3dc2f459
angel3124 <Angel.31978@stu.upes.ac.in>
2026-08-28 13:53:46 +0530

    Add files via upload

---
+++ prog1_day4.c
@@ -0,0 +1,40 @@
+/*Q7: Write a program to swap two numbers without using a third variable.
+
+
+Sample Test Cases:
+Input 1:
+10 20
+Output 1:
+After swap: 20 10
+
+Input 2:
+7 14
+Output 2:
+After swap: 14 7
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    printf("After swapping: a = %d, b = %d\n", a, b);
+
+    return 0;
+}
+
+/*C:\Users\WELCOME\OneDrive\Desktop\C_file>
+C:\Users\WELCOME\OneDrive\Desktop\C_file>gcc prog1_day4.c -o prog1_day4.out
+
+C:\Users\WELCOME\OneDrive\Desktop\C_file>prog1_day4.out
+Enter two numbers: 10 20
+After swapping: a = 20, b = 10
+*\
